bd33beda16400e6c508f10f8471852aa7f60e8c9
Nathanael PINEAU <npineau@student.42.fr>
2015-10-28 19:37:18 +0100

    it typecheck, at least I got that

--- AStar.hs
+++ AStar.hs
@@ -2,48 +2,60 @@ import Data.PSQueue as PSQ
 import Data.Map     as Map
 import Data.Set     as Set
 
-aStarSearch goal start =
-    runAStar PSQ.singleton start (heuristic start) (Set.empty) (Map.singleton start Nothing)
-
-runAStar :: AStar a cost -> [] a
-runAStar aStar = do
-    (current :-> _, o) <- PSQ.minView (opened aStar)
-    if (goal aStar) current
-       then backtrack (path aStar) current
-       else let (aStar, _) = Set.fold eval (aStar {opened = o, closed = Set.insert current (closed aStar)}, current) ((expand aStar) current)
-             in runAStar aStar
-
 data AStar a cost = AStar {
     expand    :: a -> Set a,
     goal      :: a -> Bool,
     opened    :: PSQ.PSQ a cost,
     closed    :: Set.Set a,
-    distance  :: Map.Map a cost,
+    distances :: Map.Map a cost,
     path      :: Map.Map a (Maybe a),
     heuristic :: a -> cost,
     cost      :: a -> a -> cost }
 
-eval :: a -> (AStar a cost, (a, cost)) -> (AStar a cost, (a, cost))
-eval n (aStar, (parent, g)) =
+aStarSearch expand cost heuristic goal start =
+    runAStar $ AStar
+    { expand    = expand
+    , opened    = PSQ.singleton start (heuristic start)
+    , closed    = Set.empty
+    , path      = Map.singleton start Nothing
+    , heuristic = heuristic
+    , goal      = goal
+    , distances = Map.empty
+    , cost      = cost }
+
+runAStar :: (Ord a, Ord cost, Num cost) => AStar a cost -> Maybe [a]
+runAStar aStar = do
+    (current :-> _, o) <- PSQ.minView (opened aStar)
+    if (goal aStar) current
+       then backtrack (path aStar) current
+       else let (aStar, _) = Set.fold eval (aStar {opened = o, closed = Set.insert current (closed aStar)}, (current, 0)) ((expand aStar) current)
+             in runAStar aStar
+
+eval :: (Ord a, Ord cost, Num cost) => a -> (AStar a cost, (a, cost)) -> (AStar a cost, (a, cost))
+eval n (aStar, t@(parent, g)) =
     let gn = g + (cost aStar) parent n
         o = opened    aStar
         c = closed    aStar
         p = path      aStar
+        d = (distances aStar)
         h = heuristic aStar in
-        case (PSQ.lookup n o, Set.member n c, Map.lookup n p) of
-          (Nothing, False, _)                       -> (aStar {
-              opened = PSQ.insert n (gn + h n) o,
-              path   = Map.insert n (parent, gn) p },
-              (parent, gn))
-          (Nothing, True,  Just(_, gp)) | gn < gp   -> (aStar {
-              opened = PSQ.insert n (gn + h n) o,
-              closed = Set.delete n            c,
-              path   = Map.insert n (parent, gn) p },
-              (parent, gn))
-          (Just(_), _,     Just(_, gp)) | gn < gp   -> (aStar {
-              opened = PSQ.adjust n (gn + h n) o,
-              path   = Map.insert n (parent, gn) p },
-              (parent, gn))
-          _                                         -> (aStar, (parent, gn))
+        case (PSQ.lookup n o, Set.member n c, Map.lookup n p, Map.lookup n (distances aStar)) of
+          (Nothing, False, _,       _)                   -> (aStar
+            { opened    = PSQ.insert n (gn + h n) o
+            , path      = Map.insert n (Just parent) p
+            , distances = Map.insert n gn d },
+              t)
+          (Nothing, True,  Just(_), Just(gp)) | gn < gp -> (aStar
+            { opened = PSQ.insert n (gn + h n) o
+            , closed = Set.delete n            c
+            , path   = Map.insert n (Just parent) p
+            , distances = Map.insert n gn d },
+              t)
+          (Just(_), _,     Just(_), Just(gp)) | gn < gp   -> (aStar
+            { opened = PSQ.adjust (\_ -> (gn + h n)) n o
+            , path   = Map.insert n (Just parent) p
+            , distances = Map.insert n gn d },
+              t)
+          _                                         -> (aStar, t)
 
 backtrack = undefined
