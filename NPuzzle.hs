8dd95477678024301bbf23c1fd5873534e497580
Nathanael PINEAU <npineau@student.42.fr>
2015-11-23 16:58:12 +0100

    slight change

--- NPuzzle.hs
+++ NPuzzle.hs
@@ -2,8 +2,8 @@ module NPuzzle where
 import Data.Array.Unboxed
 import Data.List
 
-solution size = fst $ go' size
+solution = fst . go
     where start n = ([], 0:[n ^ 2 - 1, n ^ 2 - 2..1])
           rotateClock = transpose . reverse
           update (s, d) n = (reverse (take n d) : rotateClock s, drop n d)
-          go' n = foldl' update (start n) (fmap (`div` 2) [2..2*n])
+          go n = foldl' update (start n) (fmap (`div` 2) [2..2*n])
