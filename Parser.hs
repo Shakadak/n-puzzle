8074568c2dd6ba08096a49592aa4996ae3a00ae5
Nathanael PINEAU <npineau@student.42.fr>
2015-11-23 18:31:13 +0100

    comment

--- Parser.hs
+++ Parser.hs
@@ -1,2 +1,25 @@
 module Parser where
-import Text.ParserCombinators.Parsec
+import Text.Parsec
+import Control.Applicative
+import Text.Parsec.String
+
+natural :: Parser Int
+natural = read <$> many1 digit
+
+lexeme :: Parser a -> Parser a
+lexeme p = do
+    x <- p
+    spaces
+    return x
+
+lexeme' :: Parser a -> Parser a
+lexeme' p = p <* spaces
+
+parseWithWSEof :: Parser a -> SourceName -> String -> Either ParseError a
+parseWithWSEof p = parseWithEof (spaces *> p)
+
+parseWithEof :: Parser a -> SourceName -> String -> Either ParseError a
+parseWithEof p = parse (p <* eof)
+
+comment :: Parser String
+comment = (:) <$> char '#' <*> manyTill anyChar newline
