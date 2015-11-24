ac2392ef09c79e2c18ba0a3913b9a1c5e8d9f55c
shakadak <nathanael.pineau@gmail.com>
2015-11-24 18:45:27 +0100

    unneeded

--- Graph.hs
+++
@@ -1,9 +0,0 @@
-module Graph where
-import Data.List
-import Data.Function
-import Data.Ord
-import Control.Arrow
-
-fromEdges :: (Eq node, Ord node) => [(node, node)] -> [(node, [node])]
-fromEdges = map shave . groupBy ((==) `on` fst) . sortBy (comparing fst)
-    where shave = first head . unzip
