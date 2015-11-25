c2a10d1a2e5282d792921579c6135ccabb26f6ba
shakadak <nathanael.pineau@gmail.com>
2015-11-25 02:37:29 +0100

    but not as huge a boost whithout the overhead of profiling, only 2.5x

--- AStar.hs
+++ AStar.hs
@@ -1,5 +1,4 @@
 module AStar (aStarSearch) where
--- import Data.PSQueue as PSQ
 import Data.HashPSQ as PSQ
 import Data.Hashable
 import Data.Map     as Map
@@ -30,36 +29,36 @@ aStarSearch expand cost heuristic goal start =
 
 runAStar :: (Hashable a, Ord a, Ord cost, Num cost) => AStar a cost -> Maybe [a]
 runAStar aStar = do
-    (k, p, current, o) <- ({-# SCC "PSQ.minView" #-} PSQ.minView (opened aStar))
+    (k, p, current, o) <- PSQ.minView (opened aStar)
     if goal aStar current
        then return $ backtrack (path aStar) current
        else let aStar' = aStar {opened = o, closed = Set.insert current (closed aStar)}
              in runAStar $ fst (Set.foldr eval (aStar', (current, fromMaybe 0 (Map.lookup current $ distances aStar'))) (expand aStar' current))
 
 eval :: (Hashable a, Ord a, Ord cost, Num cost) => a -> (AStar a cost, (a, cost)) -> (AStar a cost, (a, cost))
-eval n (aStar, t@(parent, f)) = {-# SCC "eval" #-}
+eval n (aStar, t@(parent, f)) =
     let fn = f + cost aStar parent n
         o = opened    aStar
         c = closed    aStar
         p = path      aStar
         d = distances aStar
         g = heuristic aStar in
-        case (({-# SCC "PSQ.lookup" #-} PSQ.lookup n o), ({-# SCC "Set.Member" #-} Set.member n c), ({-# SCC "Map.lookupPath" #-} Map.lookup n p), ({-# SCC "Map.lookupDistance" #-} Map.lookup n (distances aStar))) of
+        case (PSQ.lookup n o, Set.member n c, Map.lookup n p, Map.lookup n (distances aStar)) of
           (Nothing, False, _,      _)                   -> (aStar
-            { opened    = ({-# SCC "PSQ.insert1" #-} PSQ.insert n (fn + g n) n o)
-            , path      = ({-# SCC "Map.insertPath1" #-} Map.insert n parent p)
-            , distances = ({-# SCC "Map.insertDistance1" #-} Map.insert n fn d) },
+            { opened    = PSQ.insert n (fn + g n) n o
+            , path      = Map.insert n parent p
+            , distances = Map.insert n fn d },
               t)
           (Nothing, True,  Just _, Just fp) | fn < fp   -> (aStar
-            { opened = ({-# SCC "PSQ.insert2" #-} PSQ.insert n (fn + g n) n o)
+            { opened = PSQ.insert n (fn + g n) n o
             , closed = Set.delete n            c
-            , path   = ({-# SCC "Map.insertPath2" #-} Map.insert n parent p)
-            , distances = ({-# SCC "Map.insertDistance2" #-} Map.insert n fn d) },
+            , path   = Map.insert n parent p
+            , distances = Map.insert n fn d },
               t)
           (Just _,  _,     Just _, Just fp) | fn < fp   -> (aStar
-            { opened = ({-# SCC "PSQ.adjust" #-} PSQ.insert n (fn + g n) n o)
-            , path   = ({-# SCC "Map.insertPath3" #-} Map.insert n parent p)
-            , distances = ({-# SCC "Map.insertDistance3" #-} Map.insert n fn d) },
+            { opened = PSQ.insert n (fn + g n) n o
+            , path   = Map.insert n parent p
+            , distances = Map.insert n fn d },
               t)
           _                                             -> (aStar, t)
 
--- Heuristics.hs
+++ Heuristics.hs
@@ -2,19 +2,9 @@ module Heuristics where
 import Data.Array
 import Data.List
 import NPuzzle
---import NPuzzleList
-import Safe
-
-
-{-manhattanList :: NPuzzleList.Grid -> NPuzzleList.Grid -> Int
-manhattanList end start = {-# SCC "manhattan" #-} sum . map (go . fst) $ start
-    where
-        manhattan' (gx, gy) (cx, cy) = abs (gx - cx) + abs (gy - cy)
-        go i = manhattan' ({-# SCC "end!i" #-} lookupJust i end) ({-# SCC "start!i" #-} lookupJust i start)
--}
 
 manhattan :: NPuzzle.Grid -> NPuzzle.Grid -> Int
-manhattan end start = {-# SCC "manhattan" #-} sum $ map go (indices start)
+manhattan end start = sum $ map go (indices start)
     where
         manhattan' (gx, gy) (cx, cy) = abs (gx - cx) + abs (gy - cy)
-        go i = manhattan' ({-# SCC "end!i" #-} end!i) ({-# SCC "start!i" #-} start!i)
+        go i = manhattan' (end!i) (start!i)
