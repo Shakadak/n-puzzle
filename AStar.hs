936db34323c1083967d923f64a73cc6e62d6af3a
shakadak <nathanael.pineau@gmail.com>
2016-10-03 22:31:26 +0200

    reworked a bit the astar search

--- AStar.hs
+++ AStar.hs
@@ -7,14 +7,10 @@ import Data.List    (unfoldr)
 import Data.Maybe   (fromMaybe)
 
 data AStar a cost = AStar {
-    expand    :: a -> Set a,
-    goal      :: a -> Bool,
     opened    :: PSQ.HashPSQ a cost a,
     closed    :: Map.Map a (),
     distances :: Map.Map a cost,
-    path      :: Map.Map a a,
-    heuristic :: a -> cost,
-    cost      :: a -> a -> cost }
+    path      :: Map.Map a a }
 
 aStarSearch :: (Num cost, Ord a, Ord cost, Hashable a) =>
      (a -> Set a)       -- expand
@@ -23,71 +19,44 @@ aStarSearch :: (Num cost, Ord a, Ord cost, Hashable a) =>
   -> (a -> Bool)        -- goal
   -> a                  -- start
   -> Maybe [a]          -- Maybe Path
-aStarSearch expand' cost' heuristic' goal' start =
-    runAStar AStar
-        { expand    = expand'
-        , opened    = PSQ.singleton start (heuristic' start) start
-        , closed    = Map.empty
-        , path      = Map.empty
-        , heuristic = heuristic'
-        , goal      = goal'
-        , distances = Map.empty
-        , cost      = cost' }
-
-runAStar :: (Num cost, Ord a, Ord cost, Hashable a) =>
-    AStar a cost -> Maybe [a]
-runAStar aStar = do
-    (_, _, current, o) <- PSQ.minView (opened aStar)
-    if goal aStar current
-       then return $ backtrack (path aStar) current
-       else let aStar' = aStar {opened = o
-                               , closed = Map.insert current () (closed aStar)}
-                neighbors = expand aStar' current
-                f = fromMaybe 0 (Map.lookup current $ distances aStar')
-             in runAStar $ fst (Set.foldr eval (aStar', (current, f)) neighbors)
-
-{-eval :: (Num cost, Ord a, Ord cost, Hashable a) =>
-    a -> (AStar a cost, (a, cost)) -> (AStar a cost, (a, cost))
-eval n (aStar, t@(parent, f)) =
-    let fn = f + cost aStar parent n
-        o = opened    aStar
-        c = closed    aStar
-        p = path      aStar
-        d = distances aStar
-        g = heuristic aStar in
-        case (PSQ.lookup n o, Map.member n c, Map.lookup n p, Map.lookup n (distances aStar)) of
-          (Nothing, False, _,      _)                   -> (aStar
-            { opened    = PSQ.insert n (fn + g n) n o
-            , path      = Map.insert n parent p
-            , distances = Map.insert n fn d },
-              t)
-          (Nothing, True,  Just _, Just fp) | fn < fp   -> (aStar
-            { opened = PSQ.insert n (fn + g n) n o
-            , closed = Map.delete n            c
-            , path   = Map.insert n parent p
-            , distances = Map.insert n fn d },
-              t)
-          (Just _,  _,     Just _, Just fp) | fn < fp   -> (aStar
-            { opened = PSQ.insert n (fn + g n) n o
-            , path   = Map.insert n parent p
-            , distances = Map.insert n fn d },
-              t)
-          _                                             -> (aStar, t)-}
+aStarSearch expand cost heuristic goal start = runAStar expand cost heuristic goal AStar
+    { opened    = PSQ.singleton start (heuristic start) start
+    , closed    = Map.empty
+    , path      = Map.empty
+    , distances = Map.empty }
 
 backtrack :: (Ord a, Hashable a) =>
     Map a a -> a -> [a]
 backtrack paths end = reverse $ end:unfoldr (\s -> fmap (\x -> (x, x)) (Map.lookup s paths)) end
 
+runAStar :: (Num cost, Ord a, Ord cost, Hashable a) =>
+     (a -> Set a)       -- expand
+  -> (a -> a -> cost)   -- cost
+  -> (a -> cost)        -- heuristic
+  -> (a -> Bool)        -- goal
+  -> AStar a cost -> Maybe [a]
+runAStar expand cost heuristic goal aStar = do
+    (_, _, current, o) <- PSQ.minView (opened aStar)
+    if goal current
+       then return $ backtrack (path aStar) current
+       else let aStar' = aStar { opened = o, closed = Map.insert current () (closed aStar) }
+                currentCost = fromMaybe 0 (Map.lookup current $ distances aStar')
+             in runAStar expand cost heuristic goal (Set.foldr (eval cost heuristic current currentCost) aStar' (expand current))
+
 eval :: (Num cost, Ord a, Ord cost, Hashable a) =>
-    a -> (AStar a cost, (a, cost)) -> (AStar a cost, (a, cost))
-eval neighbor skip@(aStar, t@(currentNode, currentCost)) =
+    (a -> a -> cost)    -- cost
+    -> (a -> cost)      -- heuristic
+    -> a                -- current node
+    -> cost             -- cost from start to current node
+    -> a                -- neighbor of current
+    -> AStar a cost -> AStar a cost
+eval cost heuristic currentNode currentCost neighbor aStar =
     if Map.member neighbor (closed aStar)
-       then skip
-       else do let tmpCost = currentCost + cost aStar currentNode neighbor
+       then aStar
+       else do let tmpCost = currentCost + cost currentNode neighbor
                if maybe False (tmpCost >) (Map.lookup neighbor (distances aStar))
-                  then skip
-                  else (aStar
-                          { opened = PSQ.insert neighbor (tmpCost + heuristic aStar neighbor) neighbor (opened aStar)
+                  then aStar
+                  else aStar
+                          { opened = PSQ.insert neighbor (tmpCost + heuristic neighbor) neighbor (opened aStar)
                           , path = Map.insert neighbor currentNode (path aStar)
                           , distances = Map.insert neighbor tmpCost (distances aStar) }
-                           , t)
